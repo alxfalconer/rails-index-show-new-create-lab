2cdd14ae161b2b48f465f09a0122b31b5555f1b7
Alexander <al.x.falconer@gmail.com>
2021-02-19 18:35:51 -0500

    commit tree b54c18c03268cf96c513668a5267163d7207b2fb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: '42069', store: 'Peoria 02')
+Coupon.create(coupon_code: '666FFF', store: 'Butzville 23')
+Coupon.create(coupon_code: 'ABBUS', store: 'Beepo 12')
